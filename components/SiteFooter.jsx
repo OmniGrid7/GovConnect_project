c65607f5099255372630901b365e0c7f9b39219c
Vishal Jha <vishaljha73563@gmail.com>
2026-09-06 11:11:29 +0530

    Update SiteFooter.jsx

--- components/SiteFooter.jsx
+++ components/SiteFooter.jsx
@@ -53,7 +53,7 @@ export default function SiteFooter() {
       </div>)}
     </div>
     <div className="footer-bottom">
-      <span>Copyright 2025 @Anti-Gravity. All rights reserved.</span>
+      <span>Copyright 2026 @Anti-Gravity. All rights reserved.</span>
       <div><Link href="/">Privacy policy</Link><Link href="/">Terms of service</Link><Link href="/">Accessibility</Link></div>
     </div>
   </footer>;
